13b4492b1c7abcc53c83f81e79dd7b736ce48971
andrewwnguyeen-netizen <andreww.nguyeen@gmail.com>
2026-07-19 10:57:02 +1000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE = 'cycle-v18';
+const FILES = [
+  './',
+  './index.html',
+  './icon-180.png',
+  './icon-120.png',
+  './manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      // Return cache first, then try network
+      return cached || fetch(e.request).then(response => {
+        // Cache fresh responses
+        if (response && response.status === 200) {
+          const clone = response.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+        }
+        return response;
+      }).catch(() => cached);
+    })
+  );
+});
